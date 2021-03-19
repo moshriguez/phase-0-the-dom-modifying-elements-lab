b9a5acce3a40f7ea494512aaf2a1f3fd20cb68bd
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-03-19 13:16:17 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.querySelector('#main');
+
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+document.body.appendChild(newHeader);
+
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Marc is the champion';
